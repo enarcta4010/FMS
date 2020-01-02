faf372709d4a378b7d267fc8f0ed364635916f99
HOMELAPTOP <pbanilad@gmail.com>
2020-01-02 13:36:51 +0800

    added algo class

---
+++ FMS/Bayes.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS
+{
+    class Bayes
+    {
+        public decimal ComputeWorthiness(decimal netincome,string UserClass,decimal monthly) {
+            decimal ans = 0, Classper=0;
+            Double per;
+            switch (UserClass) {
+                case "A":
+                    per = 0.1;
+                    Classper =(decimal)per;
+                break;
+                case "B":
+                    per = 0.75;
+                    Classper = (decimal)per;
+                    break;
+                case "C":
+                    per = 0.5;
+                    Classper = (decimal)per;
+                    break;
+                case "D":
+                    per = 0.25;
+                    Classper = (decimal)per;
+                    break;
+                default:
+                    Classper = 0;
+                    break;
+
+            }
+            decimal PH = netincome / monthly;
+            decimal PEH1 = (PH + Classper) / 2;
+            decimal PEH2 = (PEH1 / Classper) * PH;
+
+            return PEH2;
+        }
+
+
+    }
+}
